689f692edabf8636e203f0989c442cfcf9c0ac60
fatih.kose <fatih.kose@hititcs.com>
2022-02-08 22:15:26 +0300

    implemented repos

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/sfgpetclinic/repositories/PetRepository.java
@@ -0,0 +1,8 @@
+package guru.springframework.sfgpetclinic.sfgpetclinic.repositories;
+
+import guru.springframework.sfgpetclinic.sfgpetclinic.model.Pet;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PetRepository extends CrudRepository<Pet, Long> {
+
+}
